605e99f2e7c9b6a0ff3b727333377df6771bd02c
jmilani224 <jmilani224@gmail.com>
2020-03-08 00:27:22 -0500

    address updated

--- src/components/loc.js
+++ src/components/loc.js
@@ -1,70 +1,75 @@
-import React from 'react'
+import React from "react"
+import { StaticQuery, graphql } from "gatsby"
 
 import locStyles from "./loc.module.css"
 import GoogleMapReact from 'google-map-react'
 
-const Loc = () => {
-    
-    return (
+const Loc = () => (
+  
+    <StaticQuery
+      query={graphql`
+        {
+          markdownRemark(frontmatter: {title: {eq: "address"}}) {
+            html
+          }
+        }
+      `}
+      render={data => 
         <div id="loc" className={locStyles.locContainer}>
-            <h1 className={locStyles.locH1}>Location</h1>
-               <div className={locStyles.main}>
-                   
-                   <div className={locStyles.left}>
-                        <div className={locStyles.conNode}>
-                            <h2 className={locStyles.head}>Address</h2>
-                            <p className={locStyles.par}>
-                            15721 Madison Ave. <br />
-                            Lakewood, Ohio 44107 <br />
-                            <a className={locStyles.phone} href="tel:(216) 339-9978">(216) 339-9978</a>
-                            </p>
-                        </div>
+        <h1 className={locStyles.locH1}>Location</h1>
+            <div className={locStyles.main}>
+                
+                <div className={locStyles.left}>
+                    <div className={locStyles.conNode} dangerouslySetInnerHTML={{__html:data.markdownRemark.html}}>
+                    </div>
 
-                        <div className={locStyles.conNode}>
-                            <h2 className={locStyles.head}>Hours</h2>
-                            <table className={locStyles.hoursTable}>
-                            <tr>
-                                <td>Sun</td>
-                                <td>Closed</td>
-                            </tr>
-                            <tr>
-                                <td>Mon - Tues</td>
-                                <td>10AM - 7PM</td>
-                            </tr>
-                            <tr>
-                                <td>Wed - Thurs</td>
-                                <td>10AM - 8PM</td>
-                            </tr>
-                            <tr>
-                                <td>Fri</td>
-                                <td>9AM - 8PM</td>
-                            </tr>
-                            <tr>
-                                <td>Sat</td>
-                                <td>9AM - 5PM</td>
-                            </tr>
-                            </table>
-                        </div> 
-                    </div>
-                    
-                    <div id="map" className={locStyles.right}>
-                            {(
-                            <GoogleMapReact
-                                bootstrapURLKeys={{ key: 'AIzaSyCWYgTnvAA3trKR7dFgpnGmBh-vAoM2OXA' }}
-                                defaultCenter={[41.4768626, -81.8081993]}
-                                defaultZoom={14}
-                            >
-                                <div
-                                className={locStyles.marker}
-                                lat={41.4768626}
-                                lng={-81.8081993}
-                                />
-                            </GoogleMapReact>
-                            )}
-                    </div>
-            </div>
+                    <div className={locStyles.conNode}>
+                        <h2 className={locStyles.head}>Hours</h2>
+                        <table className={locStyles.hoursTable}>
+                        <tr>
+                            <td>Sun</td>
+                            <td>Closed</td>
+                        </tr>
+                        <tr>
+                            <td>Mon - Tues</td>
+                            <td>10AM - 7PM</td>
+                        </tr>
+                        <tr>
+                            <td>Wed - Thurs</td>
+                            <td>10AM - 8PM</td>
+                        </tr>
+                        <tr>
+                            <td>Fri</td>
+                            <td>9AM - 8PM</td>
+                        </tr>
+                        <tr>
+                            <td>Sat</td>
+                            <td>9AM - 5PM</td>
+                        </tr>
+                        </table>
+                    </div> 
+                </div>
+                
+                <div id="map" className={locStyles.right}>
+                        {(
+                        <GoogleMapReact
+                            bootstrapURLKeys={{ key: 'AIzaSyCWYgTnvAA3trKR7dFgpnGmBh-vAoM2OXA' }}
+                            defaultCenter={[41.4768626, -81.8081993]}
+                            defaultZoom={14}
+                        >
+                            <div
+                            className={locStyles.marker}
+                            lat={41.4768626}
+                            lng={-81.8081993}
+                            />
+                        </GoogleMapReact>
+                        )}
+                </div>
         </div>
-    )
-}
 
-export default Loc
+      </div>}
+    ></StaticQuery>
+
+  )
+  
+  export default Loc
--- src/markdown-pages/Location/address.md
+++ src/markdown-pages/Location/address.md
@@ -2,8 +2,10 @@
 title: "address"
 ---
 
-15721 Madison Ave.
-Lakewood, Ohio 44107
+## Address
+
+15721 Madison Ave.  
+Lakewood, Ohio 44107  
 [(216) 339-9978](tel:(216) 339-9978)
 
 
