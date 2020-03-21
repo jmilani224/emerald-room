5ea0017759e357709f8c7fdf7b1991d566b8f905
jmilani224 <jmilani224@gmail.com>
2020-03-21 00:27:11 -0400

    cms

--- src/components/about.js
+++ src/components/about.js
@@ -7,15 +7,21 @@ const About = () => (
   
     <StaticQuery
       query={graphql`
-        {
-          markdownRemark(frontmatter: {title: {eq: "about"}}) {
-            html
+      {
+        markdownRemark(frontmatter: {title: {eq: "about"}}) {
+          frontmatter {
+            h1
+            p1
+            p2
           }
         }
+      }
       `}
       render={data => 
-      <div id="about" className={aboutStyles.container} dangerouslySetInnerHTML={{__html:data.markdownRemark.html}}>
-
+      <div id="about" className={aboutStyles.container}>
+        <h1>{data.markdownRemark.frontmatter.h1}</h1>
+        <p>{data.markdownRemark.frontmatter.p1}</p>
+        <p>{data.markdownRemark.frontmatter.p2}</p>
       </div>}
     ></StaticQuery>
 
