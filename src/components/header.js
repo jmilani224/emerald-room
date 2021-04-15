a19b6cbb84157914e9ddd7e04edc7d5df7d1937f
jmilani224 <jmilani224@gmail.com>
2021-04-15 11:02:35 -0400

    made book button a link

--- src/components/header.js
+++ src/components/header.js
@@ -5,39 +5,42 @@ import headerStyles from "./header.module.css"
 
 const Header = () => {
 
-const [navBackground, setNavBackground] = useState({opaque:false})
+  const [navBackground, setNavBackground] = useState({ opaque: false })
 
-const navOnScroll = () => {
-    if (window.scrollY > 200){
-    setNavBackground({opaque:true})
-  } else {
-    setNavBackground({opaque:false})
+  const navOnScroll = () => {
+    if (window.scrollY > 200) {
+      setNavBackground({ opaque: true })
+    } else {
+      setNavBackground({ opaque: false })
+    }
   }
-}
 
-useEffect(() => {
-  window.addEventListener('scroll', navOnScroll)
-}, [])
+  useEffect(() => {
+    window.addEventListener('scroll', navOnScroll)
+  }, [])
 
-    return (
-      <div 
-        className={headerStyles.stickyContainer}>
-        <header className={navBackground.opaque ? headerStyles.headerOpaque : headerStyles.headerClear}>
-          <div className={headerStyles.title} onClick={() => scrollTo('#home')} onKeyDown={() => scrollTo('#home')}>
-            THE EMERALD ROOM
+  const href = "https://www.vagaro.com//Users/BusinessWidget.aspx?enc=MMLjhIwJMcwFQhXLL7ifVGhtNR386VJFxo/FS1ttYn7Wgk+7oAMT5Geh0JTfFvoMF3Q1SGkkKk1qRUfkdVHtBLUIoJw7Xuq1kbJ8y2HD2SHPO8ogWU+ErJoJhwoPj0vH+ZDc4RO5wpCspFnDBWzjkxfHMH+N14R49Pi5NnWO6KS8THnSG8K65s7mmygc4k1vpmrUG/R8c5sbelNOWfYbstNXLoKphreXm5Lio+ffBSmKsodlrVN0s7lcXkCgR4EciGgiPL/GAU3jpWNaDJP58lZgxKMrqSS9nRWO7gQpNGia5cIpDMKJzkmqE2jgKmcr/7+IMOw7FSuIpt3svOSTKg=="
+
+  return (
+    <div
+      className={headerStyles.stickyContainer}>
+      <header className={navBackground.opaque ? headerStyles.headerOpaque : headerStyles.headerClear}>
+        <div className={headerStyles.title} onClick={() => scrollTo('#home')} onKeyDown={() => scrollTo('#home')}>
+          THE EMERALD ROOM
           </div>
         <nav>
           <ul className={headerStyles.navList}>
-              <li className={headerStyles.navItem} onClick={() => scrollTo('#home')} onKeyDown={() => scrollTo('#home')}>Home</li>
-              <li className={headerStyles.navItem} onClick={() => scrollTo('#about')} onKeyDown={() => scrollTo('#about')}>About</li>
-              <li className={headerStyles.navItem} onClick={() => scrollTo('#services')} onKeyDown={() => scrollTo('#services')}>Services</li>
-              <li className={headerStyles.navItem} onClick={() => scrollTo('#book')} onKeyDown={() => scrollTo('#book')}>Book Now</li>
-              <li className={headerStyles.navItem} onClick={() => scrollTo('#loc')} onKeyDown={() => scrollTo('#loc')}>Location</li>
+            <li className={headerStyles.navItem} onClick={() => scrollTo('#home')} onKeyDown={() => scrollTo('#home')}>Home</li>
+            <li className={headerStyles.navItem} onClick={() => scrollTo('#about')} onKeyDown={() => scrollTo('#about')}>About</li>
+            <li className={headerStyles.navItem} onClick={() => scrollTo('#services')} onKeyDown={() => scrollTo('#services')}>Services</li>
+            {/* <li className={headerStyles.navItem} onClick={() => scrollTo('#book')} onKeyDown={() => scrollTo('#book')}>Book Now</li> TEMP COMMENT WHILE COMPONENT IS FIXED*/}
+            <li className={headerStyles.navItem} ><a href={href}>Book Now</a></li>
+            <li className={headerStyles.navItem} onClick={() => scrollTo('#loc')} onKeyDown={() => scrollTo('#loc')}>Location</li>
           </ul>
         </nav>
       </header>
-      </div>
-    )
+    </div>
+  )
 }
 
 export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect} from "react"
+import React, { useState, useEffect } from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
 import '../styles/index.css'
-import {Helmet} from "react-helmet"
+import { Helmet } from "react-helmet"
 import icon from "../../static/admin/favicon.png"
 
 import Header from "../components/header"
@@ -26,51 +26,51 @@ const IndexPage = () => {
     }
     `)
 
-    const [cookie, setCookie] = useState({isCookie:true})
-    const newCookie = `visited=true; max-age=${data.markdownRemark.frontmatter.expirationInDays * 60 * 60 * 24}`; 
-    
+    const [cookie, setCookie] = useState({ isCookie: true })
+    const newCookie = `visited=true; max-age=${data.markdownRemark.frontmatter.expirationInDays * 60 * 60 * 24}`;
+
     useEffect(() => {
         if (!document.cookie.split(';').some((item) => item.trim().startsWith('visited='))) { //check to see if a cookie has been placed, if not this is a 'first visit'
-            setCookie({isCookie:false});
+            setCookie({ isCookie: false });
             document.cookie = newCookie; //place cookie on first visit
         }
-      }, [])
+    }, [])
 
-      const [widgetLoaded, setWidgetLoaded] = useState(false)
+    const [widgetLoaded, setWidgetLoaded] = useState(false)
 
-      useEffect(() => {
-          const vigaroWidget = document.querySelector('vagaro-container')
-          setWidgetLoaded(!!vigaroWidget)
-      }, [])
+    useEffect(() => {
+        const vigaroWidget = document.querySelector('vagaro-container')
+        setWidgetLoaded(!!vigaroWidget)
+    }, [])
 
-      console.log(widgetLoaded)
+    console.log(widgetLoaded)
 
-      return (
-            <div>
-                <Helmet>
-                    <meta charSet="utf-8" />
-                    <title>The Emerald Room</title>
-                    <meta name="description" content="A salon in Lakewood, Ohio, offering hair, spa, and other services for everyone." />
-                    <link rel="icon" 
-                        type="image/png" 
-                        href={icon}>
-                    </link>
-                </Helmet>
+    return (
+        <div>
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>The Emerald Room</title>
+                <meta name="description" content="A salon in Lakewood, Ohio, offering hair, spa, and other services for everyone." />
+                <link rel="icon"
+                    type="image/png"
+                    href={icon}>
+                </link>
+            </Helmet>
 
-                <PopUp cookie={cookie.isCookie}/>
-                
-                <Header />
+            <PopUp cookie={cookie.isCookie} />
 
-                <Hero />
-                
-                <About />
+            <Header />
 
-                <Services />
+            <Hero />
 
-                <Book />
+            <About />
 
-                <Loc />
-            </div>
+            <Services />
+
+            {/* <Book /> TEMP COMMENT WHILE COMPONENT IS FIXED*/}
+
+            <Loc />
+        </div>
     )
 }
 
